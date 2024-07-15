786c2b8ab4fe4e8f90e5abdc32c87f75c84fd0e7
subhaz18 <psubhash2004@gmail.com>
2024-07-15 14:16:52 +0530

    arrastrptr

---
+++ strarrypointer.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int* myf(char *str){
+    int i=0;
+    while(str[i]!='\0'){
+        if(str[i]=='a')
+        str[i]='z';
+        i++;
+    }
+    return str;
+}
+int main(){
+    char str="prepinsta";
+    int* p;
+    p=myf(str);
+    printf("%s",p);
+    return 0;
+}
