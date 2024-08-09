338d0270ac0c67d0dfadf52a1ea0c6b4ce001661
subhaz18 <psubhash2004@gmail.com>
2024-08-09 22:17:34 +0530

    find the kth element form the last of the linked list

---
+++ find_kth_element_from_the_laast_in_linked_list.cpp
@@ -0,0 +1,106 @@
+#include<iostream>
+using namespace std;
+struct node
+{
+    int num;
+    node *nextptr;
+};node* stnode = nullptr // Initialize stnode to nullptrvoid make(int n)
+{
+    node *frntNode, *tmp;
+    int num, i;
+    stnode = new node;
+    if(stnode == nullptr)
+    {
+        cout<<"Memory can not be allocated";
+        return;
+    }
+    else
+    {
+        cout<<"Enter the data for node 1: ";
+        cin>>num;
+        stnode->num = num;
+        stnode->nextptr = nullptr; // Initialize next pointer to nullptr
+        tmp = stnode;
+        for(i=2; i<=n; i++)
+        {
+            frntNode = new node;
+            if(frntNode == nullptr)
+            {
+                cout<<"Memory can not be allocated";
+                break;
+            }
+            else
+            {
+                cout<<"Enter the data for node "<>num;
+                frntNode->num = num;
+                frntNode->nextptr = nullptr;
+                tmp->nextptr = frntNode;
+                tmp = tmp->nextptr;
+            }
+        }
+    }
+}node* getkthLastNode(node* stnode, int k, int n)
+{
+    node *front, *back;
+    int i;
+    front = back = stnode;
+    if(k > n)
+    {
+        cout << "\nk is greater than the size of Linked List\n";
+        return nullptr;
+    }
+    for(i = 0; i < k-1; i++)
+    {
+        front = front->nextptr;
+    }
+    while(front->nextptr != nullptr)
+    {
+        front = front->nextptr;
+        back = back->nextptr;
+    }
+    return back;
+}void print()
+{
+    node *tmp;
+    if(stnode == nullptr)
+    {
+        cout<<"List is empty";
+    }
+    else
+    {
+        tmp = stnode;
+        cout<<"Linked List\t";
+        while(tmp != nullptr)
+        {
+            cout<num<<"\t";
+            tmp = tmp->nextptr;
+        }
+    }
+}int main()
+{
+    int n, k;
+    cout<<"Enter the number of nodes: ";
+    cin>>n;
+    make(n);
+    cout<<"\nLinked list data: \n";
+    print();
+    cout<<"\nEnter the value of k:";
+    cin>>k;
+    cout<< endl;
+    node* result = getkthLastNode(stnode, k, n);
+    if(result != nullptr)
+    {
+        cout << "Kth node from the end: " << result->num << endl;
+    }
+    else
+    {
+        cout << "Invalid input for k." << endl;
+    }    // Free the allocated memory
+    node* current = stnode;
+    while(current != nullptr)
+    {
+        node* next = current->nextptr;
+        delete current;
+        current = next;
+    }
+}
