0530b62e78cb373b693bcc1c27be3c35d3df7a5c
subhaz18 <psubhash2004@gmail.com>
2024-08-14 17:46:14 +0530

    demo

---
+++ demo.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+
+
+struct Node  
+{ 
+  int data; 
+  struct Node *left; 
+  struct Node *right; 
+  Node(int val){
+      data=val;
+      left=right=NULL;
+  }
+};
+queue<Node*> q;
+int leftview(Node *node){
+    if(node == NULL)
+        return 1;
+    q.push(node);
+    if(node->left!=NULL)
+    return leftview(node->left);
+    else
+    return leftview(node->right);
+    
+}
+void printleftview(){
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        cout<<temp->data<<" ";
+        q.pop();
+    }
+    
+}
+int main() {
+	
+	Node *root=new Node(20);
+	root->left=new Node(70);
+	root->right=new Node(90);
+	root->left->left=new Node(30);
+	root->left->right=new Node(40);
+	root->right->right=new Node(70);
+	leftview(root);
+    printleftview();
+}
