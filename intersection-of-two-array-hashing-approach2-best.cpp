2c5f04660513d18d74ff64d98851a0b98e4414bb
subhaz18 <psubhash2004@gmail.com>
2024-08-25 21:16:46 +0530

    intersection of two array using hashing approach 2 best in cpp o(n)

---
+++ intersection-of-two-array-hashing-approach2-best.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector <int> getIntersection(int arr1[], int arr2[], int size1, int size2, vector<int> &result)
+{
+    unordered_set<int> set1;
+    unordered_set<int> set2;
+
+    //This input taking can be optimized further
+    //check below
+    for (int i = 0; i < size1; i++)
+        set1.insert(arr1[i]);
+
+    for (int i = 0; i < size2; i++)
+        set2.insert(arr2[i]);
+
+    // int i = 0, j = 0;
+
+    // // add elements till size1/size2, whichever is smaller
+    // while(i<size1 && j<size2){
+    //     set1.insert(arr1[i]);
+    //     set2.insert(arr2[j]);
+    //     i++;
+    //     j++;
+    // }
+
+    // Optimized method to add elements to set
+    // // add remaining elements of arr1, if any
+    // while(i<size1){
+    //     set1.insert(arr1[i]);
+    //     i++;
+    // }
+
+    //     // add remaining elements of arr1, if any
+    // while(j<size2){
+    //     set2.insert(arr2[j]);
+    //     j++;
+    // }
+
+
+    for (int i = 0; i < size2; i++) {
+        if (set1.count(arr2[i])) {
+            result.push_back(arr2[i]);
+            set1.erase(arr2[i]);
+        }
+    }
+
+
+    return result;
+}
+
+int main()
+{
+    int arr1[] = {5, 8, 5, 7, 8, 10};
+    int arr2[] = {1, 5, 5, 8, 1, 8, 7};
+    vector <int> result;
+
+    int size1 = sizeof(arr1) / sizeof(arr1[0]);
+    int size2 = sizeof(arr2) / sizeof(arr2[0]);
+
+    getIntersection(arr1, arr2, size1, size2, result);
+
+    for (auto x : result)
+        cout << x << " ";
+
+
+    return 0;
+}
